bcf5bfe01c3332ab1e09f440c42d455d63d30856
edidiesky <essienedidiong1000@gmail.com>
2024-11-02 20:00:39 -0700

    Still Making changes to the landing page interactivity

--- frontend/src/app/(landing_pages)/_components/WhyChoose.tsx
+++ frontend/src/app/(landing_pages)/_components/WhyChoose.tsx
@@ -5,16 +5,16 @@ import AnimateTextWord from '@/components/common/AnimateTextWord';
 import { whyChooseUs } from '@/constants';
 
 const WhyChoose = () => {
-    return <div className='min-h-[500px] lg:min-h-[550px] py-16 flex items-center gap-8 justify-center'>
+    return <div className='min-h-[500px] lg:min-h-[550px] py-16 flex lg:items-center gap-8 lg:justify-center'>
         <div className="max-w-custom mx-auto w-[90%] grid grid-cols-1 gap-12">
-            <div className="w-full flex flex-col items-center justify-center gap-8">
+            <div className="w-full flex flex-col lg:items-center lg:justify-center gap-8">
                 <span className='text-base lg:text-lg font-normal lg:text-center max-w-[500px]'>
                     <AnimateTextWord type='bigtext_Center'>
                         Our Features
                     </AnimateTextWord>
                 </span>
                 <div className="flex flex-col gap-4 lg:items-center lg:justify-center ">
-                    <h2 className="text-4xl lg:text-7xl max-w-[900px] mx-auto lg:text-center leading-[1.2] family2 text-dark family2">
+                    <h2 className="text-4xl lg:text-7xl max-w-[900px] lg:mx-auto lg:text-center leading-[1.2] family2 text-dark family2">
                         <AnimateTextWord type='bigtext_Center'>
                             Innovative Features of Phoneblock
                         </AnimateTextWord>
@@ -28,11 +28,11 @@ const WhyChoose = () => {
                     </span>
                 </div>
             </div>
-            <div className="w-full flex flex-col items-center justify-center gap-4">
+            <div className="w-full flex flex-col lg:items-center lg:justify-center gap-4">
                 <ol className="lg:w-[90%] grid mt-4 lg:grid-cols-3 gap-4 lg:gap-6">
                     {
                         whyChooseUs?.map((data, index) => {
-                            return <div key={index} className="w-full flex flex-col justify-center gap-8 min-h-[260px] lg:min-h-[310px]  border p-6 rounded-2xl">
+                            return <div key={index} className="w-full flex flex-col justify-center items-start gap-8 min-h-[260px] lg:min-h-[310px]  border p-6 rounded-2xl">
                                 {
                                     data.icon
                                 }
@@ -45,7 +45,7 @@ const WhyChoose = () => {
                                         </AnimateTextWord>
 
                                     </span>
-                                    <span className="block text-sm md:etxt-base py-3 font-normal">
+                                    <span className="block text-base md:text-lg py-3 font-normal">
 
                                         <AnimateTextWord>
                                             {data?.subtext}
